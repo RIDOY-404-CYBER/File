2b86eafc9e0aa989755112c022f2bb611b98260f
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2023-02-01 23:32:59 +0600

    Update File.py

--- File.py
+++ File.py
@@ -1,2 +1,11 @@
-import RMXXD6
-RMXXD6.RM()
+import os,platform
+os.system('clear')
+print('[•] Checking Updates...')
+os.system('git pull')
+bit = platform.architecture()[0]
+if bit=='64bit':
+    print('\033[1;32m[•] Congrats! Your Device Support This Tools')
+    os.system('xdg-open https://facebook.com/groups/770617227293870/')
+    import RMXXD6
+else:
+    exit('\033[38;196m[×] Sorry Device Not Support')
