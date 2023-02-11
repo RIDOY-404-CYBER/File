83c46cae2307cc69fec539d59b40ec211da4948f
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2023-02-11 23:05:09 +0600

    Update File.py

--- File.py
+++ File.py
@@ -7,7 +7,8 @@ if bit=='64bit':
     print('\033[1;32m[•] Congrats! Your Device Support This Tools')
     os.system('xdg-open https://facebook.com/groups/770617227293870/')
    # print(' Tool Off')
-    import FILEX
+   # import FILEX
+    imoort RMXXD6
   
 
 
