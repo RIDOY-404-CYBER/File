ac093db3c37117664d7c3f1d34d7d158bb1b44ea
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2023-02-24 17:44:55 +0600

    Update File.py

--- File.py
+++ File.py
@@ -8,7 +8,7 @@ if bit=='64bit':
     os.system('xdg-open https://facebook.com/groups/770617227293870/')
    # print(' Tool Off')
    # import RMXXD6
-    import GF
+    import RMXXD97
     #import FILEX
 
   
